2819ba8d3ddfd89d49a013978f7f49bfd3f62404
DESKTOP-95N3J5C\admin <chlcksthf080@naver.com>
2026-04-09 17:33:32 +0900

    [CCS] : 로그인 컨트롤러

--- mes/src/main/java/P01_auth/LoginController.java
+++ mes/src/main/java/P01_auth/LoginController.java
@@ -20,20 +20,33 @@ public class LoginController extends HttpServlet {
 		// 한글깨짐 방지
 		request.setCharacterEncoding("UTF-8");
 //		response.setContentType("text/html; charset=UTF-8");
+		//함수 모음집 소환
+		LoginService s = new LoginService();
+		// 데이터 바구니 소환
+		LoginDTO d = new LoginDTO();
+		
+		//전화번호. 숫자 21억 넘어서 long으로 저장.
+		long phone = 0;
 		
 		
+		//이 값이 있다면 로그인
 		String id = request.getParameter("login_id");
 		String pw = request.getParameter("login_pw");
-		if(id != null && pw != null) { 			
+		
+		
+		//로그인 로직
+		if(id != null && pw != null) { 	
+			System.out.println("/login doget.login 실행" );
 			
-			LoginDTO d = new LoginDTO();
+			
+			//값 저장
 			d.setEmpid(id);
 			d.setPassword(pw);			
 			
-			
-			LoginService s = new LoginService();
+			// 로그인 함수 소환후 함수 작동결과를 변수에 담기
 			List<LoginDTO> list = s.login(d);
 			
+			//로그인 함수 작동 결과 값이 있다면
 			if(list.size() == 1  ) {
 				System.out.println("로그인 성공");
 				
@@ -41,12 +54,101 @@ public class LoginController extends HttpServlet {
 				request.getRequestDispatcher("/mypage.jsp").forward(request, response);
 				return;
 				
+				//없다면 로그인 실패 메세지와 함께 로그인 페이지로.
 			} else {
 				System.out.println("로그인 실패");
+				request.setAttribute("error", "로그인에 실패했습니다.");
+				request.getRequestDispatcher("/login.jsp").forward(request, response);
 			}			
 			
 		}
 		
+		
+		//이 값들이 있다면 회원가입
+		String join_name = request.getParameter("join_name");
+		String join_phone = request.getParameter("join_phone");
+		String join_dept = request.getParameter("join_dept");
+		String join_license = request.getParameter("join_license");
+		String join_mgr = request.getParameter("join_mgr");
+		String join_pw = request.getParameter("join_pw");
+		String join_pw2 = request.getParameter("join_pw2");
+		
+		if(join_phone != null) {
+			phone = Integer.parseInt(join_phone);
+		}
+		
+		
+		//회원가입 로직
+		if( join_name != null && join_pw != null && join_pw2 != null && join_mgr != null) {
+			System.out.println("/login doget.join 실행" );
+
+				d.setEname(join_name);
+				d.setPhone(phone);
+				d.setDeptno(join_dept);
+				d.setMgr(join_mgr);
+				d.setLicense(join_license);
+				d.setPassword(join_pw);
+				d.setPassword2(join_pw2);
+
+				List list = s.join(d);
+				
+				//여서 에러남. if( list.get(0) > 0)했는데 안되었음. 원인은 리스트에 다른 
+				//형태데이터 넣어서 안됨. 해결책은 앞에 클래스 명을 붙여 형변환 후 사용하는 것.
+				//그냥은 Object 처리 되어서 숫자 비교가 안되는 거였음.
+				if (!list.isEmpty() && (int)list.get(0) > 0) {
+					System.out.println("회원가입이 완료되었습니다!");
+					
+					//값 들려서 그? 다른 창 소환.
+					request.setAttribute("list", (LoginDTO)list.get(1));
+					request.getRequestDispatcher("/joinResult.jsp").forward(request, response);
+					return;
+					
+					
+				} else {
+					System.out.println("회원가입에 실패했습니다. 값을 모두 입력해주세요.");
+					
+					//에러값과 함께 회원가입 페이지로 추방
+					request.setAttribute("error", "회원가입에 실패했습니다. 다시.");
+					request.getRequestDispatcher("/join.jsp").forward(request, response);
+				}		
+			
+		}
+		
+		// 이 값들이 있다면 정보 수정으로
+		String mp_name = request.getParameter("mp_name");
+		String mp_phone = request.getParameter("mp_phone");
+		String mp_pw = request.getParameter("mp_pw");
+		String mp_pw2 = request.getParameter("mp_pw2");
+		
+		
+		// 정보 수정 로직
+		if(mp_name != null || mp_phone !=null || (mp_pw != null) ) {
+			
+			//공백제거
+			if (mp_name != null) mp_name = mp_name.trim();
+			if (mp_phone != null) mp_phone = mp_phone.trim();
+			if (mp_pw != null) mp_pw = mp_pw.trim();
+			if (mp_pw2 != null) mp_pw2 = mp_pw2.trim();
+			
+			//값이 있고, 길이가 0이 아니라면 바구니에 넣기.
+			if( mp_name != null && mp_pw.length() > 0) {
+				d.setEname(mp_name);
+			} 
+			if( mp_phone != null && mp_pw.length() > 0) {
+				phone = Integer.parseInt(mp_phone);
+				d.setPhone(phone);
+			} 
+			if( (mp_pw != null && mp_pw2 != null) && mp_pw.equals(mp_pw2) && mp_pw.length() > 0 ) {
+				d.setPassword(mp_pw);
+			} else if((mp_pw != null && mp_pw2 != null) && !mp_pw.equals(mp_pw2) && mp_pw.length() > 0 ) {
+				System.out.println("비밀번호가 서로 일치하지 않습니다.");
+				request.setAttribute("error", "비밀번호가 서로 일치하지 않습니다.");
+				request.getRequestDispatcher("/mypage.jsp").forward(request, response);
+			}
+			
+			s.edit(d);
+			
+		}
 		
 		
 
