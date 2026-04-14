41ff72e753b30641c50abf0a977c056ee8894234
DESKTOP-95N3J5C\admin <chlcksthf080@naver.com>
2026-04-14 17:36:09 +0900

    [CCS] : 권한 상세 컨트롤러

---
+++ mes/src/main/java/P01_auth/PermissionDetailController.java
@@ -0,0 +1,58 @@
+package P01_auth;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/pdetail")
+public class PermissionDetailController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("permission의 doget 실행");
+		// TODO Auto-generated method stub
+
+		// 주소 : http://localhost:8080/mes/login.jsp
+
+		// 한글깨짐 방지
+		request.setCharacterEncoding("UTF-8");
+//		response.setContentType("text/html; charset=UTF-8");
+		// 함수 모음집 소환
+		LoginService s = new LoginService();
+		// 데이터 바구니 소환
+		LoginDTO d = new LoginDTO();
+		
+		
+		//Paging 페이징
+		String empid = request.getParameter("empid");	
+		
+
+		
+		d = s.detail(empid);
+		
+	
+		// 세션 소환
+		HttpSession session = request.getSession();
+
+		// 세션으로 보내기
+		session.setAttribute("d", d);
+
+		// 세션이니 그냥 주소 바뀌게 ㄱㄱ.
+		request.getRequestDispatcher("/perdetail.jsp").forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
