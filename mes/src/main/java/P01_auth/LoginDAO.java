8419599106a23f621e347d7f03aa676503de1cf8
DESKTOP-95N3J5C\admin <chlcksthf080@naver.com>
2026-04-14 17:33:25 +0900

    [CCS] : 로그인 DAO

--- mes/src/main/java/P01_auth/LoginDAO.java
+++ mes/src/main/java/P01_auth/LoginDAO.java
@@ -596,4 +596,307 @@ public class LoginDAO {
 		return list;
 		
 	}
+	
+	
+	
+	public LoginDTO detail(String empid) {
+		System.out.println("/login DAO.detail 실행");
+
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		// 바구니 소환
+		LoginDTO dto = new LoginDTO();
+
+		try {
+			// JNDI 방식
+			// connection.xml 맨 아래에 있는 DB정보로 커넥션 풀을 가져온다. Server 폴더에 있다. 기억!
+			Context ctx = new InitialContext();
+
+			// DataSource : 커넥션 풀 관리자
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+
+			// DB접속(그런데 이제 커넥션 풀로.)
+			conn = dataFactory.getConnection();
+
+			// SQL 준비
+			String query = " select * from user_info u ";
+			query += " left outer join dept d on u.dept_no = d.dept_no ";
+			query += " where u.emp_id = ? ";
+			
+
+			ps = conn.prepareStatement(query);
+			ps.setString(1, empid);
+			
+
+			// SQL 실행 및 결과 확보
+			rs = ps.executeQuery();
+
+			// 결과 활용
+
+			// 신입사원에게 알려줄 내용들
+			while (rs.next()) {
+				dto.setEname(rs.getString("ename"));
+				dto.setEmpid(rs.getString("emp_id"));				
+				dto.setDeptname(rs.getString("DEPT_NAME"));
+				
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+		System.out.println("Login 함수 실행 : " + dto.toString());
+		return dto;
+
+	}
+	
+	
+	public int permission(LoginDTO d) {
+		System.out.println("/login DAO.permission 실행");
+		
+		int count = -1;
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		
+		try {
+			// JNDI 방식
+			// connection.xml 맨 아래에 있는 DB정보로 커넥션 풀을 가져온다. Server 폴더에 있다. 기억!
+			Context ctx = new InitialContext();
+			
+			// DataSource : 커넥션 풀 관리자
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+			
+			// DB접속(그런데 이제 커넥션 풀로.)
+			conn = dataFactory.getConnection();
+			
+			// SQL 준비
+			String query = " update user_info ";
+			query += " set auth = ? ";
+			query += " where emp_id = ? ";
+			
+			
+			ps = conn.prepareStatement(query);
+			ps.setInt(1, d.getAuth());
+			ps.setString(2, d.getEmpid());
+			
+			
+			// SQL 실행 및 결과 확보
+			count = ps.executeUpdate();
+			
+			// 결과 활용
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		System.out.println("권한 변경 결과 : " + count + " (0이상이면 성공. -1이면 실패) ");
+		return count;
+		
+	}
+	
+	
+	
+	public List<DashDTO> defect() {
+		System.out.println("/dashboard DAO.defect 실행");
+		
+		List<DashDTO> list = new ArrayList<DashDTO>();
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			// JNDI 방식
+			// connection.xml 맨 아래에 있는 DB정보로 커넥션 풀을 가져온다. Server 폴더에 있다. 기억!
+			Context ctx = new InitialContext();
+			
+			// DataSource : 커넥션 풀 관리자
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+			
+			// DB접속(그런데 이제 커넥션 풀로.)
+			conn = dataFactory.getConnection();
+			
+			// SQL 준비
+			String query = " SELECT * FROM defect d ";
+				   query += " LEFT OUTER JOIN defect_type t ON d.dtype_no = t.dtype_no ";
+				 
+			
+			ps = conn.prepareStatement(query);
+			
+			
+			// SQL 실행 및 결과 확보
+			rs = ps.executeQuery();
+			
+			// 결과 활용
+			
+			while (rs.next()) {
+				DashDTO dto = new DashDTO();
+				
+				//바구니에 담기
+				dto.setDtype_name(rs.getString("dtype_name"));				
+				
+				//바구니를 리스트에 싣기
+				list.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		System.out.println("defect 함수 실행 : " + list.size());
+		return list;
+		
+	}
+	
+	
+	public List<DashDTO> notice() {
+		System.out.println("/dashboard DAO.notice 실행");
+		
+		List<DashDTO> list = new ArrayList<DashDTO>();
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			// JNDI 방식
+			// connection.xml 맨 아래에 있는 DB정보로 커넥션 풀을 가져온다. Server 폴더에 있다. 기억!
+			Context ctx = new InitialContext();
+			
+			// DataSource : 커넥션 풀 관리자
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/oracle");
+			
+			// DB접속(그런데 이제 커넥션 풀로.)
+			conn = dataFactory.getConnection();
+			
+			// SQL 준비
+			String query = " SELECT boardno, title FROM announcement ";
+			       query += " order by boardno desc ";
+				 
+			
+			ps = conn.prepareStatement(query);
+			
+			
+			// SQL 실행 및 결과 확보
+			rs = ps.executeQuery();
+			
+			// 결과 활용
+			
+			while (rs.next()) {
+				DashDTO dto = new DashDTO();
+				
+				//바구니에 담기
+				dto.setBoardno(rs.getString("boardno"));				
+				dto.setTitle(rs.getString("title"));				
+				
+				//바구니를 리스트에 싣기
+				list.add(dto);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		System.out.println("notice 함수 실행 : " + list.size());
+		return list;
+		
+	}
+	
+	
 }
